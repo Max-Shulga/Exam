f983a102f9204ade733f4861a79fa346a54d03eb
Max Shulga <shulgamackim@gmail.com>
2022-09-19 09:37:23 +0300

    4 task added

--- C_code/Program.cs
+++ C_code/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿bool RepetitionCheck(int index,int[] temp)
+{
+    bool check = true;
+    for (int i = 0; i < temp.Length; i++)
+    {
+        if (index == temp[i])
+        check = false;
+    }
+    return check;
+}
+
+string[] FormatArray(string[] array)
+{
+    int newArrayLenght = new Random().Next(1,4);
+    int[] temp = new int[newArrayLenght];
+    for (int i = 0; i < temp.Length; i++)
+    {
+        temp[i] = -1;
+    }
+    string[] finalArray = new string[newArrayLenght];
+    for (int i = 0; i < newArrayLenght; i++)
+    {
+        int positionInFirstArray = new Random().Next(0,array.Length);
+        while(!RepetitionCheck(positionInFirstArray,temp))
+        {
+            positionInFirstArray = new Random().Next(0,array.Length);
+        }
+        finalArray[i] = array[positionInFirstArray];
+        temp[i] = positionInFirstArray;
+    }
+    return finalArray;
+}
+
+string[] array = {"double","string", "int", "char", "float", "bool", "car", "object"};
+string[] finalArray = FormatArray(array);
+Console.WriteLine($"[{String.Join(",", finalArray)}]");
